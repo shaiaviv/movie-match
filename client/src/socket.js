bdf4f8d6c7fafda32813b44d625828bcdaddc427
shaiaviv <shaizxzx@gmail.com>
2026-03-03 14:45:25 +0200

    Use env variable for server URL (deployment prep)

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001', { autoConnect: false });
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || 'http://localhost:3001';
+
+const socket = io(SERVER_URL, { autoConnect: false });
 
 export default socket;
